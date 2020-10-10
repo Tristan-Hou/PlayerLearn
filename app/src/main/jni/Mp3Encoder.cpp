6aab00325ee91a85f0dcdfdc884976a2fef8d3ef
Tristan-Hou <houjh123@gmail.com>
2020-10-10 20:00:35 +0800

    compile success.

--- app/src/main/jni/Mp3Encoder.cpp
+++ app/src/main/jni/Mp3Encoder.cpp
@@ -1,11 +1,6 @@
-//#include <stdio.h>
-//#include <jni.h>
-//#include <android/log.h>
-
-//#include "Mp3Encoder.h"
-#include "../../../../../../Library/Android/sdk/ndk/20.1.5948944/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/jni.h"
-//#include "../../../../../../Library/Android/sdk/ndk/20.1.5948944/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/android/log.h"
-#include "../../../../../../Library/Android/sdk/ndk/20.1.5948944/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/android/log.h"
+#include <stdio.h>
+#include "Mp3Encoder.h"
+#include <android/log.h>
 
 #define LOG_TAG "dodoodla_crash"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -14,5 +9,5 @@
 extern "C" JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_encode
   (JNIEnv *, jobject) {
 //  LOGI("encoder encode");
-    ALOGD("===============crrrrash================");
+    ALOGD("===============crash================");
   }
