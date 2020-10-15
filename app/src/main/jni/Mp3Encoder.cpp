70226548fe4f8de5664b308f7a867268239756ae
Tristan-Hou <houjh123@gmail.com>
2020-10-16 00:24:53 +0800

    add init/destroy method.

--- app/src/main/jni/Mp3Encoder.cpp
+++ app/src/main/jni/Mp3Encoder.cpp
@@ -6,8 +6,18 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
-extern "C" JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_encode
-  (JNIEnv *, jobject) {
-//  LOGI("encoder encode");
-    ALOGD("===============crash================");
+extern "C" JNIEXPORT jint JNICALL Java_com_phuket_tour_studio_Mp3Encoder_init
+        (JNIEnv *, jobject, jstring, jint, jint, jint, jstring) {
+    ALOGD("===============init================");
+    return 0;
   }
+
+extern "C" JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_encode
+        (JNIEnv *, jobject) {
+    ALOGD("===============encode================");
+}
+
+extern "C" JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_destroy
+        (JNIEnv *, jobject) {
+    ALOGD("===============destroy================");
+}
--- app/src/main/jni/Mp3Encoder.h
+++ app/src/main/jni/Mp3Encoder.h
@@ -10,11 +10,27 @@ extern "C" {
 #endif
 /*
  * Class:     com_phuket_tour_studio_Mp3Encoder
+ * Method:    init
+ * Signature: (Ljava/lang/String;IIILjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_com_phuket_tour_studio_Mp3Encoder_init
+        (JNIEnv *, jobject, jstring, jint, jint, jint, jstring);
+
+/*
+ * Class:     com_phuket_tour_studio_Mp3Encoder
  * Method:    encode
  * Signature: ()V
  */
 JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_encode
-  (JNIEnv *, jobject);
+        (JNIEnv *, jobject);
+
+/*
+ * Class:     com_phuket_tour_studio_Mp3Encoder
+ * Method:    destroy
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_com_phuket_tour_studio_Mp3Encoder_destroy
+        (JNIEnv *, jobject);
 
 #ifdef __cplusplus
 }
